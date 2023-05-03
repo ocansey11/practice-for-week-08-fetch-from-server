8d2c0b14b2d709771052eb60d2fc4a24f165742a
Kevin Ocansey <kevinocansey11@gmail.com>
2023-05-03 13:14:25 +0100

    completed fetch exercise

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,59 @@
 export function getAllDogs() {
     // Your code here
+   return fetch("/dogs")  
 }
 
 export function getDogNumberTwo() {
     // Your code here
+   return fetch("/dogs/2");
 }
 
 export function postNewDog() {
     // Your code here
+    const url = "/dogs"
+    const headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    const body = new URLSearchParams({
+        name: 'elka',
+        age : 4
+    }); 
+    const options = {
+        method : "POST",
+        headers: headers,
+        body: body
+    }
+
+   return fetch(url, options)
 }
 
 export function postNewDogV2(name, age) {
      // Your code here
+
+    const url = "/dogs"
+    const headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    const body = new URLSearchParams({
+        name: `${name}`,
+        age : `${age}`
+    }); 
+    const options = {
+        method : "POST",
+        headers: headers,
+        body: body
+    }
+ 
+    return fetch(url, options)
+
 }
 
 export function deleteDog(id) {
       // Your code here
+      const url = `/dogs/${id}/delete`;
+      const headers = {"AUTH": "ckyut5wau0000jyv5bsrud90y"};
+      const body = "";
+      const options = {
+          method: "POST",
+          headers: headers,
+          body: body
+      };
+  
+      return fetch(url, options);
 }
